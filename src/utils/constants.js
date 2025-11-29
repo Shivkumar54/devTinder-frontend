ec8129d291c55ed02b0df9dc48e79d6840ff536c
Shivkumar M <mshivkumar59@gmail.com>
2025-11-29 19:42:25 +0530

    updated base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 export const DEFAULT_IMAGE =
   "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg";
 
-export const BASE_URL = "http://localhost:3500";
+  // BASE URL for develpoment - "http://localhost:3500";
+export const BASE_URL = "/api/";
